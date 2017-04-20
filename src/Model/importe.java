a4d55030cf669743f8190aef46ec08f6eb20e400
davidblancolopez <daviddbl94@gmail.com>
2017-04-20 21:03:25 +0200

    Cambios

--- src/Model/importe.java
+++ src/Model/importe.java
@@ -4,5 +4,28 @@ package Model;
 public class importe {
     double importe;
     Matricula matricula;
+
+    public importe(double importe, Matricula matricula) {
+        this.importe = importe;
+        this.matricula = matricula;
+    }
+
+    public double getImporte() {
+        return importe;
+    }
+
+    public void setImporte(double importe) {
+        this.importe = importe;
+    }
+
+    public Matricula getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(Matricula matricula) {
+        this.matricula = matricula;
+    }
+    
+    
     
 }
