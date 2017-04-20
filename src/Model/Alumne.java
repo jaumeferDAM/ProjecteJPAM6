758b1d360d48b6eb7892475fbf8732ab57368458
davidblancolopez <daviddbl94@gmail.com>
2017-04-20 19:13:18 +0200

    Cambios

--- src/Model/Alumne.java
+++ src/Model/Alumne.java
@@ -3,6 +3,60 @@ package Model;
 
 
 public class Alumne {
+    String nif;
+    String nom;
+    String cognom;
+    String correu;
+    String matricula;
+
+    public Alumne(String nif, String nom, String cognom, String correu, String matricula) {
+        this.nif = nif;
+        this.nom = nom;
+        this.cognom = cognom;
+        this.correu = correu;
+        this.matricula = matricula;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCognom() {
+        return cognom;
+    }
+
+    public void setCognom(String cognom) {
+        this.cognom = cognom;
+    }
+
+    public String getCorreu() {
+        return correu;
+    }
+
+    public void setCorreu(String correu) {
+        this.correu = correu;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    
     
     
 }
