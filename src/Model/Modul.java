9a0badd74e8d515a1dc118b7cc8deaad43ffe966
davidblancolopez <daviddbl94@gmail.com>
2017-04-25 20:18:35 +0200

    Cambios

--- src/Model/Modul.java
+++ src/Model/Modul.java
@@ -1,26 +1,45 @@
 
 package Model;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.ArrayList;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-
-public class Modul {
+@Entity
+@Table(name = "Modul")
+public class Modul implements Serializable{
     
-    String id;
+    private static final long serialVersionUID = 1L; 
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idModul", unique = true, nullable = false)
+    private int id;
+    
+    @Column(name = "nomModul")
     String nom;
     ArrayList <UnitatFormativa> llistaUnitatsFormatives;
 
-    public Modul(String id, String nom, ArrayList<UnitatFormativa> llistaUnitatsFormatives) {
+    public Modul(int id, String nom, ArrayList<UnitatFormativa> llistaUnitatsFormatives) {
         this.id = id;
         this.nom = nom;
         this.llistaUnitatsFormatives = llistaUnitatsFormatives;
     }
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(int id) {
         this.id = id;
     }
 
