f324768b3ccd11ef88c62d011389cd2a9b2113ec
jaumeferDAM <jaumefernandezacanmas@gmail.com>
2017-04-28 16:50:24 +0200

    blablabla

--- src/Controlador/Modul_Controlador.java
+++ src/Controlador/Modul_Controlador.java
@@ -1,7 +1,40 @@
 
 package Controlador;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 
-public class Modul_Controlador {
+
+public class Modul_Controlador extends GENERICODAOImpl<Object, Serializable>{
+
+    @Override
+    public ArrayList<Object> listarTodos() {
+        return super.listarTodos(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void eliminar(Serializable id) {
+        super.eliminar(id); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object get(Serializable id) {
+        return super.get(id); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void actualizar(Object entity) {
+        super.actualizar(entity); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void insertar(Object entity) {
+        super.insertar(entity); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object crear() {
+        return super.crear(); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
