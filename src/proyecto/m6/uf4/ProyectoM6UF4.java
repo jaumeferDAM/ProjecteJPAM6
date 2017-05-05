a312fc7f0a2019c0362d9a0a334b17009507b733
jaumeferDAM <jaumefernandezacanmas@gmail.com>
2017-05-05 19:54:26 +0200

    prubea

--- src/proyecto/m6/uf4/ProyectoM6UF4.java
+++ src/proyecto/m6/uf4/ProyectoM6UF4.java
@@ -9,14 +9,14 @@ public class ProyectoM6UF4 {
 
 
     public static void main(String[] args) {
-        Alumne_Controlador alumneDAO = new Alumne_Controlador();
-        
-        Alumne alumne1 = new Alumne("111111", "nom", "cognom", "correu", 555555);
-        alumneDAO.crear();
-        alumneDAO.insertar(alumne1);
-        alumneDAO.actualizar(new Alumne("111111", "Juan", "Alberto", "correu", 555555));
+//        Alumne_Controlador alumneDAO = new Alumne_Controlador();
+//        
+//        Alumne alumne1 = new Alumne("111111", "nom", "cognom", "correu", 555555);
+//        alumneDAO.crear();
+//        alumneDAO.insertar(alumne1);
+//        alumneDAO.actualizar(new Alumne("111111", "Juan", "Alberto", "correu", 555555));
 //        alumneDAO.eliminar(alumne1);
-        alumneDAO.buscarCognom("Alberto");
+//        alumneDAO.buscarCognom("Alberto");
     }
     
 }
